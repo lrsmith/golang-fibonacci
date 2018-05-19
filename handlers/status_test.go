145e5ee60397c049e90b4a9acb03f2530de484b5
Len Smith <lrsmith@umich.edu>
2018-05-19 10:52:01 -0700

    Adjust test

--- handlers/status_test.go
+++ handlers/status_test.go
@@ -17,12 +17,12 @@ func TestStatusHandler(t *testing.T) {
 	handler := http.HandlerFunc(Status)
 	handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusInternalServerError {
+	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("Status handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
 
-	expected := `{"httpstatus":500,"status":"dead"}`
+	expected := `{"httpstatus":200,"status":"healthy"}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
