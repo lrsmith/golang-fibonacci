8c070621ab44acb0aa0ff0e22722f76dee89dc3f
Len Smith <lrsmith@umich.edu>
2018-05-19 19:50:18 -0700

    Added very, very basic authentication. But framework is in place to tie into other wuth

--- main.go
+++ main.go
@@ -10,9 +10,14 @@ import (
 
 func main() {
 
+	amw := authenticationMiddleware{}
+	amw.Populate()
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/status", handlers.Status)
 	router.HandleFunc("/v1/fibseq", handlers.FibSeq)
+
+	router.Use(amw.Middleware)
 
 	log.Fatal(http.ListenAndServeTLS(":8443", "./config/server.crt", "./config/server.key", router))
 
